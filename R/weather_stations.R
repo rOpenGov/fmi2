62b5267c1e4ec6e69c88f4c0819c17f1acc0e2a3
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2018-08-13 21:02:09 +0300

    Get FMI weather stations
    Migrate old code from fmi to download FMI station data from the web. Also update tests and a local copy.

---
+++ R/weather_stations.R
@@ -0,0 +1,133 @@
+# function fmi_station()
+
+# Reading the local version included within the package is separated into its
+# own function, since it's also used for tests.
+.fmi_stations_local <- function() {
+  message("Station list downloaded")
+  system.file("extdata", "fmi_stations.csv", package = "fmi2") %>%
+    utils::read.csv(as.is = TRUE) %>%
+    tibble::as_tibble()
+}
+
+# Declare globalVariables to prevent check from complaining about
+# NSE
+utils::globalVariables(c("Elevation"))
+
+# Use a closure for function fmi_station() in order to cache the results.
+.fmi_stations_closure <- function() {
+
+  cached_stations <- NULL
+
+  function(groups = NULL, quiet = FALSE) {
+
+    stations <- NULL
+
+    if (!is.null(cached_stations)) {
+      stations <- cached_stations
+      message("Using cached stations")
+    } else {
+      tryCatch({
+        station_url <- "http://en.ilmatieteenlaitos.fi/observation-stations"
+        stations <- xml2::read_html(station_url) %>%
+          rvest::html_table() %>%
+          `[[`(1L) %>%
+          tibble::as_tibble() %>%
+          dplyr::mutate(
+            Elevation = Elevation %>% sub(pattern = "\n.*$", replacement = "") %>%
+          as.integer())
+
+        # Groups can contain multiple values, but html_table() and
+        # readHTMLable() both lose the separating '<br />'. Since group names
+        # seem to start with an uppercase letter, use that to separate them.
+        # It seems that the order in which they are returned can vary, so
+        # sort them in alphabetical order to get consistent results
+        # (important for the test that checks whether the included local copy
+        # is still up-to-date with the online version).
+        stations$Groups <- stations$Groups %>%
+          sub(pattern = "([a-z])([A-Z])", replacement = "\\1;\\2") %>%
+          strsplit(";") %>%
+          lapply(sort) %>%
+          lapply(paste, collapse = ", ") %>%
+          unlist()
+        cached_stations <<- stations
+        if (!quiet) {
+          message("Station list downloaded from ", station_url)
+        }
+      }, error = function(e) {
+        if (!quiet) {
+          message("Error downloading from ", station_url)
+        }
+      })
+    }
+    if (is.null(stations)) {
+      if (!quiet) {
+        message("Using local copy instead.")
+      }
+      stations <- .fmi_stations_local()
+    }
+    if (!is.null(groups)) {
+      indexes <- lapply(groups, grep, x = stations$Groups) %>%
+        unlist() %>%
+        sort() %>%
+        unique()
+      stations <- stations[indexes, ]
+    }
+    return(stations)
+  }
+}
+#' Get a list of active FMI observation stations.
+#'
+#' A table of active observation stations is downloaded from the website of
+#' Finnish Meteorological Institute, if package \pkg{rvest} or package \pkg{XML}
+#' is installed. If neither is, or if the download fails for any other reason, a
+#' local copy provided as a csv file within the \pkg{fmi} package is used.
+#'
+#' \code{fmi_weather_stations()} is a deprecated alias for
+#' \code{fmi_stations(groups="Weather stations")}.
+#'
+#' @param groups a character vector of observation station groups to subset for
+#' @param quiet whether to suppress printing of diagnostic messages
+#'
+#' @return a \code{data.frame} of active observation stations
+#'
+#' @seealso \url{http://en.ilmatieteenlaitos.fi/observation-stations}
+#'
+#' @author Joona Lehtomaki \email{joona.lehtomaki@@gmail.com},
+#' Ilari Scheinin
+#'
+#' @importFrom magrittr %>%
+#' @import rvest
+#' @import xml2
+#'
+#' @export
+#'
+#' @aliases fmi_weather_stations
+#'
+fmi_stations <- .fmi_stations_closure()
+
+#' Check if a provided ID number is a valid FMI SID.
+#'
+#' \code{fmisid} is a ID numbering system used by the FMI.
+#'
+#' @param fmisid numeric or character ID number.
+#'
+#' @return logical
+#'
+#' @seealso \code{\link{fmi_stations}}
+#'
+#' @author Joona Lehtomaki \email{joona.lehtomaki@@gmail.com}
+#' @export
+#'
+valid_fmisid <- function(fmisid) {
+  if (is.null(fmisid)) {
+    return(FALSE)
+  } else {
+    fmisid <- as.numeric(fmisid)
+    stations <- fmi_stations()
+    if (fmisid %in% stations$FMISID) {
+      return(TRUE)
+    } else {
+      return(FALSE)
+    }
+  }
+}
--- tests/testthat/test_50_fmi_stations.R
+++ tests/testthat/test_50_fmi_stations.R
@@ -1,20 +1,23 @@
 library(dplyr)
-library(fmi)
+library(fmi2)
 context("FMI station list")
 
 test_that("downloading works", {
+  # First, time should download the data
   expect_message(stations <- fmi_stations(), "Station list downloaded")
+  # If done again immediately, should use cached version
+  expect_message(stations <- fmi_stations(), "Using cached stations")
 })
 
 test_that("local version is up-to-date", {
-  local_stations <- fmi:::.fmi_stations_local()
+  local_stations <- fmi2:::.fmi_stations_local()
   downloaded_stations <- fmi_stations()
 
   # for some reason this fails with tibbles, so convert to regular data.frames
-  downloaded_stations <- as.data.frame(downloaded_stations)
-  local_stations <- as.data.frame(local_stations)
+  #downloaded_stations <- as.data.frame(downloaded_stations)
+  #local_stations <- as.data.frame(local_stations)
 
-  expect_equal(local_stations, downloaded_stations, info = paste0(
+  expect_equivalent(local_stations, downloaded_stations, info = paste0(
     "The observation station list on the FMI website has been updated, ",
     "and the local copy needs to be updated with:\n",
     'write.csv(fmi_stations(), file="inst/extdata/fmi_stations.csv", row.names=FALSE)'))
