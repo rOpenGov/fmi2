b96cdcebf99510989fe0b303e91dbb959e2730c6
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-05-06 18:48:51 +0300

    Test for succesful getFeature request

--- tests/testthat/test_01_api.R
+++ tests/testthat/test_01_api.R
@@ -4,6 +4,11 @@ httptest::with_mock_api({
     expect_error(fmi_api(request = "foobar"),
                  "^Invalid request type")
     expect_is(fmi_api(request = "DescribeStoredQueries"), "fmi_api")
+    # This should work
+    expect_is(fmi_api(request = "getFeature",
+                      storedquery_id = "fmi::observations::weather::daily::timevaluepair",
+                      starttime = "2019-01-01", endtime = "2019-01-04",
+                      fmisid = 100946), "fmi_api")
   })
 
   test_that("Non-existing path causes a well-behaving error", {
