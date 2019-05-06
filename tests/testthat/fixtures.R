55310b6eb1302661c3a44ca390c38ffe6c56dd81
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-05-06 18:47:06 +0300

    Update fixtures

--- tests/testthat/fixtures.R
+++ tests/testthat/fixtures.R
@@ -5,9 +5,14 @@ library(httr)
 .mockPaths("tests/testthat")
 
 dsq_obj <- fmi_api("DescribeStoredQueries")
+dat_obj <- fmi_api(request = "getFeature",
+                   storedquery_id = "fmi::observations::weather::daily::timevaluepair",
+                   starttime = "2019-01-01", endtime = "2019-01-04",
+                   fmisid = 100946)
 
 httptest::start_capturing()
 
 httr::GET(dsq_obj$url)
+httr::GET(dat_obj$url)
 
 httptest::stop_capturing()
