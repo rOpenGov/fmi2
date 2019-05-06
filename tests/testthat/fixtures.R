fa695d8a454516bbb3eee862564657e7e432b44e
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-05-06 18:17:05 +0300

    Update fixtures
    Both code and data.

--- tests/testthat/fixtures.R
+++ tests/testthat/fixtures.R
@@ -1,12 +1,13 @@
+library(fmi2)
 library(httptest)
 library(httr)
 
 .mockPaths("tests/testthat")
 
+dsq_obj <- fmi_api("DescribeStoredQueries")
+
 httptest::start_capturing()
 
-# NOTE: this is not the most elegant use of the namespace, but since it's not
-# exported...
-httr::GET(fmi2:::fmi2_global$saved_queries_url)
+httr::GET(dsq_obj$url)
 
 httptest::stop_capturing()
