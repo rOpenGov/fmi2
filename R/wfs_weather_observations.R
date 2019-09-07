bb158a3f6b5a0b786e98c9eef38c3e4784eda9fc
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-09-07 13:29:26 +0300

    Implement obs_weather_hourly()

--- R/wfs_weather_observations.R
+++ R/wfs_weather_observations.R
@@ -59,3 +59,68 @@ obs_weather_daily <- function(starttime, endtime, fmisid = NULL) {
     dplyr::mutate(value = ifelse(is.nan(value), NA, value))
   return(sf_obj)
 }
+
+#' Hourly weather observations from weather stations.
+#'
+#' Default set contains hourly air temperature average, maximum and minimum, air
+#' relative humidity average, wind speed average, minumum (10 minute average)
+#' and maximum (10 minute average), wind direction average, wind gust speed
+#' maximum (3 second average), rain accumulated, rain intensity maximum, air
+#' pressure average and the most significant weather code. By default, the data
+#' is returned from last 24 hours. At least one location parameter
+#' (geoid/place/fmisid/wmo/bbox) has to be given.
+#'
+#' The FMI WFS stored query used by this function is
+#' `fmi::observations::weather::hourly::simple`. For more informations, see
+#' [the FMI documentation page](https://en.ilmatieteenlaitos.fi/open-data-manual-fmi-wfs-services).
+#'
+#' @param starttime character begin of the time interval in ISO-format.
+#' @param endtime character end of time interval in ISO-format.
+#' @param timestep numeric the time step of data in minutes.
+#' @param parameters character vector of parameters to return (see below).
+#' @param crs character coordinate projection to use in results.
+#' @param bbox numeric vector (EXAMPLE) bounding box of area for which to return
+#'        data.
+#' @param place character location name for which to provide data.
+#' @param fmisid numeric FMI observation station identifier
+#'        (see \link[fmi2]{fmi_stations}.
+#' @param	maxlocations numeric maximum amount of locations.
+#' @param geoid numeric geoid of the location for which to return data.
+#' @param wmo numeric WMO code of the location for which to return data.
+#'
+#' @import dplyr
+#'
+#' @note For a complete description of the accepted arguments, see
+#' `list_parameters("fmi::observations::weather::hourly::simple")`.
+#'
+#' @return sf object in a long (melted) form. Observation variables names are
+#' given in `variable` column. Following variables are returned:
+#'   \describe{
+#'     \item{rrday}{Precipitation amount}
+#'     \item{snow}{Snow depth}
+#'     \item{tday}{Average air temperature}
+#'     \item{tmin}{Minimum air temperature}
+#'     \item{tmax}{Maximum air temperature}
+#'     \item{TG_PT12H_min}{Ground minimum temperature}
+#'   }
+#'
+#' @export
+#'
+#' @seealso https://en.ilmatieteenlaitos.fi/open-data-manual-fmi-wfs-services,
+#' @seealso \link[fmi2]{list_parameters}
+#'
+obs_weather_hourly <- function(starttime, endtime, fmisid = NULL) {
+  fmi_obj <- fmi_api(request = "getFeature",
+                     storedquery_id = "fmi::observations::weather::hourly::simple",
+                     starttime = starttime, endtime = endtime, fmisid = fmisid)
+  sf_obj <- to_sf(fmi_obj)
+  sf_obj <- sf_obj %>%
+    dplyr::select(time = Time, variable = ParameterName,
+                  value = ParameterValue) %>%
+    dplyr::mutate(time = as.Date(time),
+                  variable = as.character(variable),
+                  # Factor needs to be coerced into character first
+                  value = as.numeric(as.character(value))) %>%
+    dplyr::mutate(value = ifelse(is.nan(value), NA, value))
+  return(sf_obj)
+}
--- tests/testthat/test_04_observations.R
+++ tests/testthat/test_04_observations.R
@@ -3,10 +3,27 @@ httptest::with_mock_api({
   test_that("daily observation data for a weather station are retrieved correctly", {
     # Use Hanko Tulliniemi weather station
     hanko_id <- 100946
-    # Instantaneous Weather Observations (fmi::observations::weather::simple)
+
     obs_dat <- obs_weather_daily(starttime = "2019-01-01",
                                  endtime = "2019-01-04",
                                  fmisid = hanko_id)
+
+    expect_is(obs_dat, "sf")
+    expect_identical(names(obs_dat), c("time", "variable", "value", "geometry"))
+    expect_is(obs_dat$time, "Date")
+    expect_is(obs_dat$variable, "character")
+    expect_is(obs_dat$value, "numeric")
+    expect_is(obs_dat$geometry, "sfc")
+  })
+
+  test_that("hourly observation data for a weather station are retrieved correctly", {
+    # Use Hanko Tulliniemi weather station
+    hanko_id <- 100946
+
+    obs_dat <- obs_weather_hourly(starttime = "2019-01-01",
+                                  endtime = "2019-01-02",
+                                  fmisid = hanko_id)
+
     expect_is(obs_dat, "sf")
     expect_identical(names(obs_dat), c("time", "variable", "value", "geometry"))
     expect_is(obs_dat$time, "Date")
