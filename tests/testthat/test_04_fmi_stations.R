a28fa86447a28fdc08cf4b771f326f73244b6eae
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-09-12 21:04:06 +0300

    Dummy load closure

--- tests/testthat/test_04_fmi_stations.R
+++ tests/testthat/test_04_fmi_stations.R
@@ -3,4 +3,12 @@ test_that("downloading works", {
   expect_message(stations <- fmi_stations(), "Station list downloaded")
   # If done again immediately, should use cached version
   expect_message(stations <- fmi_stations(), "Using cached stations")
+
+  expect_is(stations, "tbl")
+
+  # A dummy call to cover the whole closure. Normally the closure is evaluated
+  # when the package is loaded, but covr will miss parts of the closure if
+  # this is not done.
+  fmi2:::.fmi_stations_closure()
+
 })
