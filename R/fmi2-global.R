58f09086fc4d7951888ed0263c7e9f462c790974
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2020-11-29 21:19:12 +0200

    Implement a new verison of fmi_stations()
    Data is retrieved over the API, not scraped from the web page.

--- R/fmi2-global.R
+++ R/fmi2-global.R
@@ -10,7 +10,10 @@ fmi2_global$base_url <- "http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0"
 # Following list keeps track on which fmi2 function wraps which stored query on
 # the FMI API.
 fmi2_global$function_map <- tibble::tibble(
-  `Stored query` = c("fmi::observations::weather::daily::simple",
+  `Stored query` = c("fmi::ef::stations",
+                     "fmi::observations::weather::daily::simple",
                      "fmi::observations::weather::hourly::simple"),
-  `fmi2 function` = c("obs_weather_daily", "obs_weather_hourly")
+  `fmi2 function` = c("fmi_stations",
+                      "obs_weather_daily",
+                      "obs_weather_hourly")
 )
