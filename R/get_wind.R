b266e2068de3ec88993f7082432c1f77d3eadbc4
Aleksi Lahtinen <allaht@utu.fi>
2024-07-03 08:46:10 +0300

    Add wrapper function get_wind

---
+++ R/get_wind.R
@@ -0,0 +1,48 @@
+#' @title Get hourly wind observations
+#' @description Retrieve hourly wind observations from weather stations.
+#'
+#' @details `get_wind()` is a wrapper function for `obs_weather_hourly()`. Function returns all wind
+#' related parameters given by `obs_weather_hourly()`. At least one location parameter has to be given.
+#' By default data is returned from the last 24 hours. The function can also
+#' label variables by using `label_variables()` and add info on station(s) by using `add_station()`.
+#'
+#'
+#' @inheritParams obs_weather_hourly
+#' @param label A logical for whether to label the variables. Default value is `FALSE`.
+#' @param station A logical for whether to add station info into the data. Default value is `FALSE`.
+#' @return sf object in a long (melted) form.
+#' @export
+#'
+#' @examples
+#'   \dontrun{
+#'     # Get wind data from Turku
+#'     y <- get_wind(place = "Turku", starttime = "2024-06-23", endtime = "2024-06-30")
+#'   }
+get_wind <- function(fmisid = NULL, place = NULL, starttime = NULL, endtime = NULL,
+                     crs = NULL, bbox = NULL, timestep = NULL, label = FALSE, station = FALSE){
+
+
+  # Get hourly data
+  y <- obs_weather_hourly(fmisid = fmisid, place = place, starttime = starttime,
+                          endtime = endtime, crs = crs, bbox = bbox, timestep = timestep,
+                          parameters = c("WD_PT1H_AVG", "WS_PT1H_AVG", "WS_PT1H_MAX", "WS_PT1H_MIN"))
+
+  # Check if variables should be labeled
+  if (label) {
+
+    y <- y %>%
+      label_variables()
+
+  }
+
+  # Check if station info should be added
+  if (station) {
+
+    y <- y %>%
+      add_station()
+
+  }
+
+  # Return the data
+  return(y)
+}
